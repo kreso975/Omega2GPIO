226b247347e400ed0c0cf2803fefcb2714235a32
Kresimir Kokanovic <kresimir.kokanovic@gmail.com>
2017-12-10 00:32:28 +0100

    added Sonar

--- source/led-blink.cpp
+++ source/led-blink.cpp
@@ -97,10 +97,10 @@ int main( int argc, char* argv[] )
                 Gpio::digitalWrite( TRIG_PIN, false );
 
                 while ( Gpio::digitalRead(ECHO_PIN) == 0 )              // Check whether the ECHO is LOW
-                   auto pulseStart = Clock::time_point<Clock::now();        // Saves the last known time of LOW pulse
+                   auto pulseStart = time_point_cast<Clock::now();        // Saves the last known time of LOW pulse
 
                 while ( Gpio::digitalRead(ECHO_PIN) == 1 )              // Check whether the ECHO is HIGH
-                   auto pulseEnd = Clock::time_point<Clock::now();          // Saves the last known time of HIGH pulse
+                   auto pulseEnd = time_point_cast<Clock::now();          // Saves the last known time of HIGH pulse
 
                 auto pulse_duration = duration_cast<microseconds>(pulseEnd - pulseStart);   // Get pulse duration to a variable
 
