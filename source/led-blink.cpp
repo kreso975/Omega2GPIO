ff26c7b964f09ba7970c3c348eac31f6e16e0559
Kresimir Kokanovic <kresimir.kokanovic@gmail.com>
2017-12-10 15:07:20 +0100

    added Sonar

--- source/led-blink.cpp
+++ source/led-blink.cpp
@@ -51,7 +51,10 @@ int main( int argc, char* argv[] )
             high_resolution_clock::time_point t2 = high_resolution_clock::now();
 
 
+            duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
 
+            std::cout << "It took me " << time_span.count() << " seconds.";
+            std::cout << std::endl;
 
             //printf ( "Vrijeme 1; %Lf", pulseStart );
 
