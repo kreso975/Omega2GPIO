89e01da6be9e204e20d9b9fedf72d2e741585849
Kresimir Kokanovic <kresimir.kokanovic@gmail.com>
2017-12-10 19:52:56 +0100

    added Sonar

--- source/led-blink.cpp
+++ source/led-blink.cpp
@@ -132,7 +132,7 @@ int main( int argc, char* argv[] )
                 //auto pulse_duration = pulseEnd - pulseStart;   // Get pulse duration to a variable
 
 
-                double distance = pulseDuration * 17150;          // Multiply pulse duration by 17150 to get distance
+                double distance = (double)pulseDuration * 17150;          // Multiply pulse duration by 17150 to get distance
                 distance = roundf( distance * 100 ) / 100;        // Round to two decimal points
 
                 /*
