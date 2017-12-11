cc47a120c7c9238c604ef69204715d63ff3c2674
Kresimir Kokanovic <kresimir.kokanovic@gmail.com>
2017-12-11 17:06:45 +0100

    added Sonar

--- source/led-blink.cpp
+++ source/led-blink.cpp
@@ -43,9 +43,10 @@ int main( int argc, char* argv[] )
 
         else if ( ( arg == "-test" )  )
         {
+            std::chrono::microseconds argvMicro = argv[2];
             high_resolution_clock::time_point t1 = high_resolution_clock::now();
 
-            std::this_thread::sleep_for(microseconds{argv[2]});
+            std::this_thread::sleep_for(microseconds{argvMicro});
 
             high_resolution_clock::time_point t2 = high_resolution_clock::now();
 
